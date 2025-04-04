a8fff529e67abbbac568c8354fbd33fa67c01122
bake <bake@192k.pw>
2025-04-04 20:13:31 +0200

    Initial Commit

---
+++ AeroMenu/AeroMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroMenuApp.swift
+//  AeroMenu
+//
+//  Created by bake on 04.04.25.
+//
+
+import SwiftUI
+
+@main
+struct AeroMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
